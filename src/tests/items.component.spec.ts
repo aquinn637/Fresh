4b2f354daa7b1469d8fb1fe9b073400c7b32a1f1
Aubrey Quinn <aquinn@softworks.com>
2020-03-19 12:10:28 +0000

    testbed test no longer working

---
+++ src/tests/items.component.spec.ts
@@ -0,0 +1,33 @@
+import { ItemsComponent } from "~/app/item/items.component";
+import { ItemService } from "~/app/item/item.service";
+import { nsTestBedBeforeEach, nsTestBedAfterEach, nsTestBedRender } from "nativescript-angular/testing";
+
+describe('Items Component Standard Test', () => {
+
+    let  component: ItemsComponent;
+
+    beforeEach(() => {
+        component = new ItemsComponent(new ItemService());
+    });
+
+    it('should be defined', () => {
+        expect(component).toBeTruthy();
+    });
+
+});
+
+describe('Renderer E2E', () => {
+
+    beforeEach(nsTestBedBeforeEach([ItemsComponent], [ItemService]));
+    afterEach(nsTestBedAfterEach(false));
+
+    it('should be defined',  () => {
+
+        nsTestBedRender(ItemsComponent).then((fixture) => {
+            expect(fixture.componentInstance).toBeTruthy();
+        });
+
+    });
+
+
+});
