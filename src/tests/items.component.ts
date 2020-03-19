ad9b696a4c05fc0b12665a57d3046cacefe86740
Aubrey Quinn <aquinn@softworks.com>
2020-03-19 11:02:15 +0000

    regular component tests work

--- src/tests/items.component.ts
+++ src/tests/items.component.ts
@@ -1,1 +1,17 @@
-//describe();
+import { ItemsComponent } from "~/app/item/items.component";
+import { ItemService } from "~/app/item/item.service";
+
+describe('', () => {
+
+    let  component: ItemsComponent;
+
+    beforeEach(() => {
+        component = new ItemsComponent(new ItemService());
+    });
+
+    it('should be defined', () => {
+        expect(component).toBeTruthy();
+    });
+
+
+});
